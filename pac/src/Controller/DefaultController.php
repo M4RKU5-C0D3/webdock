bf13e556c4cd2f289e901ed62e884335de087e12
Markus Martens <markus.martens@gmx.net>
2020-10-30 21:52:54 +0100

    add some sub pages

--- pac/src/Controller/DefaultController.php
+++ pac/src/Controller/DefaultController.php
@@ -9,21 +9,47 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
+    const MENU1 = [
+        'Dashboard' => '',
+        'Environment' => 'env',
+        'Docker' => 'docker',
+    ];
+    const  MENU2 = [
+        'hub.docker.com' => 'https://hub.docker.com/r/m4rku5/webdock',
+        'github.com' => 'https://github.com/M4RKU5-C0D3/webdock',
+    ];
+
     /**
      * @Route("/")
      */
     public function index(MessageGenerator $messageGenerator): Response
     {
         return $this->render('base.html.twig', [
-            'menu1' => [
-                'Environment' => '#',
-                'Docker' => '#',
-            ],
-            'menu2' => [
-                'hub.docker.com' => 'https://hub.docker.com/r/m4rku5/webdock',
-                'github.com' => 'https://github.com/M4RKU5-C0D3/webdock',
-            ],
+            'menu1' => self::MENU1,
+            'menu2' => self::MENU2,
             'message' => $messageGenerator->getHappyMessage(),
+        ]);
+    }
+
+    /**
+     * @Route("/env")
+     */
+    public function environment(): Response
+    {
+        return $this->render('environment.html.twig', [
+            'menu1' => self::MENU1,
+            'menu2' => self::MENU2,
+        ]);
+    }
+
+    /**
+     * @Route("/docker")
+     */
+    public function docker(): Response
+    {
+        return $this->render('docker.html.twig', [
+            'menu1' => self::MENU1,
+            'menu2' => self::MENU2,
         ]);
     }
 }

3006e140114d38db236c8cb4b1bc52b9d2dc32b2
Markus Martens <markus.martens@gmx.net>
2020-10-30 22:17:46 +0100

    refactor template

--- pac/src/Controller/DefaultController.php
+++ pac/src/Controller/DefaultController.php
@@ -2,53 +2,43 @@
 
 namespace App\Controller;
 
-use App\Service\MessageGenerator;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
-    const MENU1 = [
-        'Dashboard' => '',
-        'Environment' => 'env',
-        'Docker' => 'docker',
-    ];
     const  MENU2 = [
         'hub.docker.com' => 'https://hub.docker.com/r/m4rku5/webdock',
         'github.com' => 'https://github.com/M4RKU5-C0D3/webdock',
     ];
 
     /**
-     * @Route("/")
+     * @Route("/", name="index")
      */
-    public function index(MessageGenerator $messageGenerator): Response
+    public function index(): Response
     {
         return $this->render('base.html.twig', [
-            'menu1' => self::MENU1,
             'menu2' => self::MENU2,
-            'message' => $messageGenerator->getHappyMessage(),
         ]);
     }
 
     /**
-     * @Route("/env")
+     * @Route("/env", name="environment")
      */
     public function environment(): Response
     {
         return $this->render('environment.html.twig', [
-            'menu1' => self::MENU1,
             'menu2' => self::MENU2,
         ]);
     }
 
     /**
-     * @Route("/docker")
+     * @Route("/docker", name="docker")
      */
     public function docker(): Response
     {
         return $this->render('docker.html.twig', [
-            'menu1' => self::MENU1,
             'menu2' => self::MENU2,
         ]);
     }
